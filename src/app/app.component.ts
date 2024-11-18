4c78a3c372ca8579e30e81f60ef2e0dc268d0618
crimson666 <ankurbsws@gmail.com>
2024-11-18 12:44:16 +0530

    signal and computed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, effect } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { NgClass, NgStyle } from '@angular/common';
+import { AsyncPipe, JsonPipe, NgClass, NgStyle } from '@angular/common';
 import { NgIf, NgFor, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { AppendPipe } from './pipes/append.pipe';
 import { UpperCasePipe } from '@angular/common';
 import { DataService } from './services/data.service';
 import { Data } from './interfaces/data';
+import { UserService } from './services/user.service';
+import { signal, computed } from '@angular/core';
+import { Todo } from './interfaces/todo';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, NgClass, NgStyle, NgIf, NgFor, NgSwitch, NgSwitchCase, NgSwitchDefault, AppendPipe, UpperCasePipe],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    NgClass,
+    NgStyle,
+    NgIf,
+    NgFor,
+    NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault,
+    AppendPipe,
+    UpperCasePipe,
+    AsyncPipe,
+    JsonPipe
+  ],
   providers: [DataService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
@@ -69,9 +86,33 @@ export class AppComponent {
   grade1: string = 'B';
 
   pipeValue: string = 'This is the main page';
+  user = this.userService.getUser();
   data: string[] = [];
-  constructor(private dataService: DataService) {
+  //Example Json slash
+  userData = {
+    id: 1,
+    name: 'shubham',
+    roles: ['admin', 'user'],
+    status: {
+      active: true,
+      lastLogin: new Date('01/03/2019')
+    }
+  };
+
+  count2 = signal<number>(0);
+  todos = signal<Todo[]>([]);
+  newTodo = signal<string>('');
+  // so computed is based on the event of a value we can return an signal, btw its a signal that has been returned
+  totalToDoItems = computed(() => this.todos().length);
+
+  constructor(private dataService: DataService, private userService: UserService) {
     this.data = this.dataService.getData();
+    //so effcts do nearly the same thisg but does not return anything
+    effect(() => {
+      const items = this.todos().length;
+      const even = items % 2 === 0 ? 'even' : 'odd';
+      console.log(`Total number of items: ${items} which is ${even}`);
+    });
   }
 
   data2: Data[] = [];
@@ -84,5 +125,27 @@ export class AppComponent {
         console.log('Error', err);
       }
     })
+  }
+
+  handleinput(event: Event) {
+    const input = event?.target as HTMLInputElement;
+    this.newTodo.set(input?.value);
+  }
+
+  addTooDo() {
+    if (this.newTodo().trim().length) {
+      const newTodoObj: Todo = {
+        id: Date.now(),
+        title: this.newTodo(),
+        done: false,
+      }
+      this.todos.set([...this.todos(), newTodoObj]);
+      this.newTodo.set('');
+    }
+  }
+
+  deleteToDo(id: number) {
+    const updatedTodos = this.todos().filter((todo) => todo?.id !== id);
+    this.todos.set(updatedTodos);
   }
 }
