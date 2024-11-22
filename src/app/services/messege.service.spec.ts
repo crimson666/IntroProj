b2e2a8b739d44506395c2b40bbfd50b1485f1154
crimson666 <ankurbsws@gmail.com>
2024-11-22 23:33:17 +0530

    stuff

--- src/app/services/messege.service.spec.ts
+++ src/app/services/messege.service.spec.ts
@@ -1,16 +1,62 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { MessegeService } from './messege.service';
+import { Post } from '../interfaces/post';
+import { languages, latestprices, orderBook, students } from '../interfaces/store';
 
 describe('MessegeService', () => {
   let service: MessegeService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [MessegeService]
+    });
+
     service = TestBed.inject(MessegeService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    // Verify there are no outstanding requests after each test
+    httpTestingController.verify();
   });
+
+  describe('getPosts', () => {
+    it('should return an array of Post objects', () => {
+      const mockPosts: Post[] = [
+        { userId: 1, id: 1, title: 'title 1', body: 'body 1' },
+        { userId: 2, id: 2, title: 'title 2', body: 'body 2' }
+      ];
+
+      service.getPosts().subscribe(posts => {
+        expect(posts).toEqual(mockPosts);
+      });
+
+      const req = httpTestingController.expectOne(service.url);
+      expect(req.request.method).toEqual('GET');
+
+      req.flush(mockPosts);
+    });
+  });
+
+  describe('getLanguagePosts', () => {
+    it('should return languages data', () => {
+      const mockLanguagesData: languages[] = [{ id: 4, name: 'English', }];
+
+      service.getLanguagePosts().subscribe(data => {
+        expect(data).toEqual(mockLanguagesData);
+      });
+
+      const req = httpTestingController.expectOne('http://localhost:3000/languages');
+      expect(req.request.method).toEqual('GET');
+
+      req.flush(mockLanguagesData);
+    });
+  });
+
+  // Similar tests for other methods (getStudentsPosts, getoutorderPosts, getlatestpricesPosts)
+  // ...
 });
--- src/app/services/messege.service.ts
+++ src/app/services/messege.service.ts
@@ -11,7 +11,7 @@ export class MessegeService {
 
   constructor(private http: HttpClient) { }
 
-  private url = 'https://jsonplaceholder.typicode.com/posts';
+  url = 'https://jsonplaceholder.typicode.com/posts';
   getPosts(): Observable<Post[]> {
     return this.http.get<Post[]>(this.url);
   }
